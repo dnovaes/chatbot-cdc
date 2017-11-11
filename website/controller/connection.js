0502a1d8df6adc4c056d22d88286c5b67987c43c
dnovaes <novaesdigo@gmail.com>
2017-11-11 12:46:56 +0000

    adicionado link de acesso externo do elasticsearch hosteado no heroku

--- website/controller/connection.js
+++ website/controller/connection.js
@@ -2,7 +2,8 @@ var es = require('elasticsearch');
 
 var client = new es.Client({
   hosts:[
-    'http://localhost:9200',
+//    'http://localhost:9200',
+    'https://c4pow5a1rd:52dfrzgn76@first-cluster-713454587.us-east-1.bonsaisearch.net'
   ],
   //log: 'trace',
   index:{
