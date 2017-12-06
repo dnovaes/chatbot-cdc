9526b6b4a3d5c9ef8fe5cc8db5a83a26a85e1b49
Tércio Souza de Jesus <terciodejesus@gmail.com>
2017-12-06 11:33:28 -0300

    Change db config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'chatbot_cdc_db'
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  user     : 'b28c5f6fe6a00e',
+  password : '64fac776',
+  database : 'heroku_a5bee640faa3e71'
 });
 
 connection.connect(function(err) {
