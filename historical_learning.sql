56f3ddd8d2ac4160d8d15aa2282dad7d211de3d7
dnovaes <novaesdigo@gmail.com>
2018-01-10 01:26:20 +0000

    adicionado campo valid_claim e alterado votos para default 0

--- historical_learning.sql
+++ historical_learning.sql
@@ -2,8 +2,9 @@ CREATE TABLE `historical_learning` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `keywords` text COLLATE utf8_unicode_ci NOT NULL,
  `article_number` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
- `vote_positive` int(11) DEFAULT NULL,
- `vote_negative` int(11) DEFAULT NULL,
+ `vote_positive` int(11) DEFAULT 0,
+ `vote_negative` int(11) DEFAULT 0,
  `historical_user_id` int(11) DEFAULT NULL,
+ `valid_claim` tinyint(1) DEFAULT 0,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
