0368836399a732dbc030d93cecadcd5a7a949384
dnovaes <novaesdigo@gmail.com>
2018-01-22 17:35:15 +0000

    adicionado campo do texto da queixa 'claim_text'

--- historical_learning.sql
+++ historical_learning.sql
@@ -2,6 +2,7 @@ CREATE TABLE `historical_learning` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `keywords` text COLLATE utf8_unicode_ci NOT NULL,
  `article_number` varchar(100) COLLATE utf8_unicode_ci DEFAULT NOT NULL,
+ `claim_text` text COLLATE utf8_unicode_ci NOT NULL,
  `vote_positive` int(11) DEFAULT 0,
  `vote_negative` int(11) DEFAULT 0,
  `valid_claim` tinyint(1) DEFAULT 0,
