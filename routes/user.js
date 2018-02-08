828659d9347aa0c6d8879f13af5eeabe1b624274
dnovaes <novaesdigo@gmail.com>
2018-02-08 22:47:42 +0000

    corrigido valor incorreto de 6 minutos para maxAge

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 
 const db = require('../config/db.js');
 const bcrypt = require('bcrypt');
-const GLOBAL_MAX_AGE = 36000; //6 min
+const GLOBAL_MAX_AGE = 360000; //6 min (1000 = 1 min)
 
 exports.signup = function(req, res) {
 
