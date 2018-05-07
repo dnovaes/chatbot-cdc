c05ae9ef836ec85f0eeb8093bac1103937672b47
dnovaes <novaesdigo@gmail.com>
2018-05-07 00:38:48 -0300

    remove file with info credentials of a private server

--- config/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-  host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'b28c5f6fe6a00e',
-  password : '64fac776',
-  database : 'heroku_a5bee640faa3e71'
-});
-
-module.exports = connection;
