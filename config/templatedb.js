518c95f540e89e87866fbda57ae461bdf988be39
dnovaes <novaesdigo@gmail.com>
2018-05-30 23:48:34 +0000

    fix the password word spelled wrong.

--- config/templatedb.js
+++ config/templatedb.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'myhostiphere.com',
   user: 'userid',
-  passowrd: 'passserial',
+  password: 'passserial',
   database: 'databasename'
 });
 
