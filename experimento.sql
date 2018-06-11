8d5b105cc841cb527cff8c7d99f3afe471189d99
dnovaes <novaesdigo@gmail.com>
2018-06-11 17:55:25 +0000

    add new table experiments for the new page 'preprexperimento' page responsible for collecting info for the tesis.

---
+++ experimento.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `experiments` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` text COLLATE utf8_unicode_ci NOT NULL,
+ `claim_id` int(11) NOT NULL,
+ `final_appointment` text COLLATE utf8_unicode_ci NOT NULL,
+ `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
