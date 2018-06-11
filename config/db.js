f9dc82b6083e95dce7f2f522a61ae103b3e1037f
dnovaes <novaesdigo@gmail.com>
2018-06-11 18:32:55 +0000

    add database credentials info

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql');
+const connection = mysql.createPool({
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b28c5f6fe6a00e',
+  password: '64fac776',
+  database: 'heroku_a5bee640faa3e71'
+});
+
+module.exports = connection;
