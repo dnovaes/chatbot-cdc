5c09be053b74b7bff59f25fcbf983e13e1debc76
dnovaes <novaesdigo@gmail.com>
2018-06-19 02:06:04 +0000

    add new column user_voter relate to the vote chosen during the experiment access

--- experimento.sql
+++ experimento.sql
@@ -4,5 +4,6 @@ CREATE TABLE `experiments` (
  `claim_id` int(11) NOT NULL,
  `final_appointment` text COLLATE utf8_unicode_ci NOT NULL,
  `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+ `user_vote` BOOLEAN NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
